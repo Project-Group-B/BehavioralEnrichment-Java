e5e69c114dce375a5f24bab544b1e6e4fcc554ac
zevans343 <zevans@unomaha.edu>
2019-04-24 00:30:01 -0500

    Added Enrichment Form retrieval

--- src/main/java/com/uno/zoo/service/EnrichmentService.java
+++ src/main/java/com/uno/zoo/service/EnrichmentService.java
@@ -18,6 +18,7 @@ import com.uno.zoo.dto.ChangePasswordForm;
 import com.uno.zoo.dto.CompleteRequestForm;
 import com.uno.zoo.dto.DepartmentInfo;
 import com.uno.zoo.dto.EditUserInfo;
+import com.uno.zoo.dto.EnrichmentForm;
 import com.uno.zoo.dto.ImageInfo;
 import com.uno.zoo.dto.ItemForm;
 import com.uno.zoo.dto.ItemInfo;
@@ -384,5 +385,18 @@ public class EnrichmentService {
 		}
 		
 		return approvedEntries;
+	}
+
+	public List<EnrichmentForm> getEnrichmentForm() {
+		List<EnrichmentForm> EnrichmentForm = new ArrayList<>();
+		
+		try {
+			EnrichmentForm = dao.getEnrichmentForm();
+		} catch(Exception e) {
+			LOGGER.error("Error getting locations:");
+			LOGGER.error(e.getMessage(), e);
+		}
+		
+		return EnrichmentForm;
 	}
 }
